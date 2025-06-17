bf9bf16c2675f0470070eaf5441d11703da9de25
Virgile <agbowagliv@gmail.com>
2025-06-17 21:41:32 +0000

    feat: Add initial layout and structure for the application
    - Created a new layout file `app.blade.php` extending from `clean.blade.php`.
    - Implemented a clean layout in `clean.blade.php` with responsive design and meta tags.
    - Added a header partial `header.blade.php` with navigation and authentication links.
    - Defined routes for the home page in `web.php`.
    - Added console command for inspiring quotes.
    - Set up .gitignore files for various storage directories to manage ignored files.
    - Created initial test cases in the `tests` directory for feature and unit testing.
    - Configured Vite for asset management with Laravel.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+
+@extends('layouts.clean')
+
+@section('childContent')
+
+    @include('layouts.partials.header')
+
+    @yield('content')
+
+    <footer></footer>
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
