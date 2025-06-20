7db0572b0ffbe2a57088ea5a2ed0f84c72fc0afb
Virgile <agbowagliv@gmail.com>
2025-06-20 17:25:34 +0000

    Refactor car detail view and update routes
    - Updated the car detail view to enhance layout and presentation, including a new image carousel and detailed specifications.
    - Changed links in car item component to use named routes for better maintainability.
    - Updated header component links to use named routes for adding new cars, signing up, and logging in.
    - Modified search form to use a named route for the action attribute.
    - Added search route and resource routes for cars in the web routes file.

---
+++ app/Models/FuelType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory;
+
+     /**
+     * Specify the database if it is different from the Model name
+     * protected $table = 'car_fuel_types';
+     */
+
+     /**
+     * Laravel assume that the primary key of every table is id but in case tou want to change it do this
+     * protected $primaryKey = 'fuel_type_id';
+     */
+
+     /**
+     *Disable auto increameting of the id
+     *public $incrementing = false;
+     */
+
+     /**
+     * Changing the data type of the primary key
+     * protected $keyType = 'string';
+     */
+
+      /**
+     * Disable timestamps which are created_at and updated_at
+     */
+    
+    /**
+     * Customize the created_at and updated-at
+     *  const CREATED_AT = "createdDate";
+     */
+    public $timestamps = false;
+   
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
@@ -13,3 +14,9 @@ Route::get('/signup',[SignupController::class, 'create'])->name('signup');
 
 //Login Routes
 Route::get('/login', [LoginController::class, 'create'])->name('login');
+
+//Search Routes
+Route::get('/car/search',[CarController::class, 'search'])->name('car.search'); 
+
+//Car Routes
+Route::resource('car', CarController::class);
